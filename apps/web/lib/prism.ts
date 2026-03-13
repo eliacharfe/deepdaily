a1d185650eeacef8e9b9c6755ba72753253fbce9
Eliachar Feig <eliacharfeig@gmail.com>
2026-03-13 13:18:50 +0200

    Improve streaming

---
+++ apps/web/lib/prism.ts
@@ -0,0 +1,10 @@
+//apps/web/src/lib/prism.ts
+
+
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-markdown";
